203f664d7f315a3b595b7e4af81f383a71be902f
satheeshchepuri <chepurusatheesh@gmail.com>
2018-08-28 23:19:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" { 
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${AWS_ACCESS_KEY}"
+  secret_key = "${AWS_SECRET_KEY}"
   region     = "${var.region}"
 }
 
